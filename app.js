c77b99885b265eadda247f9947e43067e27c53e7
Leo Santillan <lcskode@gmail.com>
2018-10-30 16:50:20 +0800

    Added partials folder that contains header.ejs and footer.ejs

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 var express = require("express");
 var app = express();
 
+// tell express to use/serve the contents of public folder
+app.use(express.static("public"));
+
+// tell Express to drop .ejs on all ejs templates
+app.set("view engine", "ejs");
+
+
 // ============================================================================
 // ROUTES
 // ============================================================================
 
 // root ROUTE
 app.get("/", function(req, res){
-  res.render("home.ejs");
+  res.render("home");
 });
 
 // fallinlovewith ROUTE
 app.get("/fallinlovewith/:thing", function(req, res){
+// app.get("/fallinlovewith", function(req, res){
   var thing = req.params.thing;
-  // pass thing variable's value to thingVar as an object
-  res.render("love.ejs", {thingVar: thing});
+  // {thingVar: thing} passes thing variable's value to thingVar as an object
+  res.render("love", {thingVar: thing});
 });
 
 // posts ROUTE
@@ -24,7 +32,7 @@ app.get("/posts", function(req, res){
     {title: "My adorable pet bunny", author: "Charlie"}, 
     {title: "Can you believe this pomsky?", author: "Colt"} 
   ];
-  res.render("posts.ejs", {posts: posts});
+  res.render("posts", {posts: posts});
 });
 
 
